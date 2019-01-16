fb8effd8db2d3cf106c5114a58859d97e497879e
nivanov <2centnik@gmail.com>
2019-01-15 22:33:50 -0500

    rtpforward started

--- js/watney_janus.js
+++ js/watney_janus.js
@@ -1,5 +1,6 @@
 var janusConnection;
-var pluginHandle;
+var streamingPluginHandle;
+var forwarderPluginHandle;
 
 function doConnect() {
     Janus.init({
@@ -22,7 +23,7 @@ function onJanusInit() {
     janusConnection = new Janus({
         server: server,
         success: function () {
-            onJanusConnect(janusConnection);
+            onJanusConnect();
         },
         error: function (error) {
             alert("Janus server error: " + error);
@@ -34,52 +35,56 @@ function onJanusInit() {
 }
 
 function onJanusConnect() {
+    attachStreamingPlugin();
+    attachForwarderPlugin();
+}
+
+function attachStreamingPlugin() {
     var opaqueId = "streamingtest-" + Janus.randomString(12);
     janusConnection.attach({
         plugin: "janus.plugin.streaming",
         opaqueId: opaqueId,
         success: function (plugin) {
-            pluginHandle = plugin;
-            onPluginAttached();
+            streamingPluginHandle = plugin;
+            streaming_onPluginAttached();
         },
         error: function (error) {
-            console.log("Error attaching plugin: " + error);
+            console.log("Error attaching streaming plugin: " + error);
         },
         onremotestream: function (stream) {
-            onRemoteStreamStart(stream);
+            streaming_onRemoteStreamStart(stream);
         },
         onmessage: function (msg, jsep) {
-            onRemoteMessage(msg, jsep);
+            streaming_onRemoteMessage(msg, jsep);
         }
     });
 }
 
-function onRemoteStreamStart(stream) {
+function streaming_onRemoteStreamStart(stream) {
     var vidEl = $("#vid").get(0);
     Janus.attachMediaStream(vidEl, stream);
-
 }
 
-function onPluginAttached() {
+function streaming_onPluginAttached() {
     // Watney HD stream is id 10
     var body = { "request": "watch", id: 10 };
-    pluginHandle.send({ "message": body });
+    streamingPluginHandle.send({ "message": body });
 }
 
-function onRemoteMessage(msg, jsep) {
+function streaming_onRemoteMessage(msg, jsep) {
     var result = msg["result"];
     if (result && result["status"] && result["status"] === 'stopped') {
-        stopStream();
+        streaming_stopStream();
     }
 
     var error = msg["error"];
     if (error) {
         console.log("Remote error: " + error);
-        stopStream();
+        streaming_stopStream();
     }
 
     if (jsep) {
-        pluginHandle.createAnswer({
+        streamingPluginHandle.createAnswer({
             jsep: jsep,
             media: {
                 audioSend: false,
@@ -88,7 +93,7 @@ function onRemoteMessage(msg, jsep) {
             },
             success: function (jsep) {
                 var body = { "request": "start" };
-                pluginHandle.send({ "message": body, "jsep": jsep });
+                streamingPluginHandle.send({ "message": body, "jsep": jsep });
             },
             error: function (error) {
                 console.log("Remote offer error: " + error);
@@ -97,10 +102,52 @@ function onRemoteMessage(msg, jsep) {
     }
 }
 
-function stopStream() {
-    if (pluginHandle) {
+function streaming_stopStream() {
+    if (streamingPluginHandle) {
         var body = { "request": "stop" };
-        pluginHandle.send({ "message": body });
-        pluginHandle.hangup();
+        streamingPluginHandle.send({ "message": body });
+        streamingPluginHandle.hangup();
     }
+}
+
+function attachForwarderPlugin() {
+    var opaqueId = "forwarder-" + Janus.randomString(12);
+    janusConnection.attach({
+        plugin: "janus.plugin.rtpforward",
+        opaqueId: opaqueId,
+        success: function (plugin) {
+            forwarderPluginHandle = plugin;
+            forwarder_onPluginAttached();
+        },
+        error: function (error) {
+            console.log("Error attaching forwarder plugin: " + error);
+        },
+        onremotestream: function (stream) {
+            //streaming_onRemoteStreamStart(stream);
+        },
+        onmessage: function (msg, jsep) {
+            forwarder_onMessage(msg, jsep);
+        }
+    });
+}
+
+function forwarder_onPluginAttached() {
+    var body = {
+        "request": "configure",
+	    "sendipv4": "127.0.0.1",
+	    "sendport_audio_rtp": 60000,
+	    "sendport_audio_rtcp": 60001,
+	    "sendport_video_rtp": 60002,
+	    "sendport_video_rtcp": 60003,     
+    };
+    forwarderPluginHandle.send({ "message": body });
+    forwarderPluginHandle.send({
+        "message": {
+            "video_enabled": false
+        }
+    });
+}
+
+function forwarder_onMessage(msg, jsep) {
+    
 }
