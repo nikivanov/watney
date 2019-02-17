9ed83a7edecc387452a5b52f6d0ec1a1927c2524
Nik Ivanov <2centnik@gmail.com>
2019-02-17 16:26:45 -0500

    fixed audio record bitrate

--- audio.sh
+++ audio.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 alsasrc device=plughw:1,0 ! mulawenc ! rtppcmupay ! udpsink host=127.0.0.1 port=8005
+gst-launch-1.0 alsasrc device=plughw:1,0 ! audio/x-raw,format=S16LE,layout=interleaved,rate=8000,channels=1 ! mulawenc ! rtppcmupay ! udpsink host=127.0.0.1 port=8005
