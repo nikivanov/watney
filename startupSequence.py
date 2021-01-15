ca55bc8db5f70117f19c9d55298b66e727e21fe3
Nik Ivanov <2centnik@gmail.com>
2021-01-14 19:26:06 -0500

    startup sequence done

--- startupSequence.py
+++ startupSequence.py
@@ -1,6 +1,8 @@
 import asyncio
 import time
+import math
 from events import Events
+from lightscontroller import LED_COUNT
 
 class StartupSequenceController:
     def __init__(self, config, servoController, lightsController, tts):
@@ -21,6 +23,7 @@ class StartupSequenceController:
         self.task = loop.create_task(self.doSequence())
 
     async def doSequence(self):
+        self.lightsController.lightsOff()
         await asyncio.sleep(1)
         servoInterval = 0.25
         self.servoController.changeServo(self.neutral)
@@ -29,21 +32,43 @@ class StartupSequenceController:
         await asyncio.sleep(servoInterval)
         self.servoController.changeServo(self.max)
         await asyncio.sleep(servoInterval)
+        
+        transitionTime = float(1)
+        startTime = time.time()
+        targetTime = startTime + transitionTime
+        servoTravel = self.max - self.neutral
+        ledFraction = float(1) / LED_COUNT
+        self.lightsController.lights.set_global_brightness(1)
+        self.lightsController.lights.clear_strip()
+        
+        while time.time() < targetTime:
+            timeFraction = (time.time() - startTime) / transitionTime
+            servoPosition = self.max - servoTravel * timeFraction
+            self.servoController.changeServo(servoPosition)
+            ledsOn = round(timeFraction / ledFraction)
+            for ix in range(0, ledsOn):
+                self.lightsController.lights.set_pixel_rgb(LED_COUNT - ix - 1, 0x002e00)
+            self.lightsController.lights.show()
+            await asyncio.sleep(0.05)
+
+
+        for ix in range(0, LED_COUNT):
+            self.lightsController.lights.set_pixel_rgb(ix, 0x002e00)
+        self.lightsController.lights.show()
         self.servoController.changeServo(self.neutral)
-        await asyncio.sleep(servoInterval)
-        self.servoController.stopServo()
-
-        lightsInterval = 0.1
-        self.lightsController.lightsOn()
-        await asyncio.sleep(lightsInterval)
-        self.lightsController.lightsOff()
-        await asyncio.sleep(lightsInterval)
-
-        self.lightsController.lightsOn()
-        await asyncio.sleep(lightsInterval)
-        self.lightsController.lightsOff()
-        await asyncio.sleep(lightsInterval)
 
         if self.greeting:
             self.tts.sayText(self.greeting)
+
+        await asyncio.sleep(0.1)
+        self.lightsController.lightsOff()
+        await asyncio.sleep(0.1)
+        for ix in range(0, LED_COUNT):
+            self.lightsController.lights.set_pixel_rgb(ix, 0x002e00)
+        self.lightsController.lights.show()
+        await asyncio.sleep(0.1)
+
+        self.servoController.stopServo()
+        self.lightsController.lights.set_global_brightness(31)
+        self.lightsController.lights.clear_strip()
         
