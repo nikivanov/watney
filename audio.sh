02262ccbf236e309edc5c39c3ff6d195ce1ebed9
Nik Ivanov <2centnik@gmail.com>
2021-01-23 15:12:14 -0500

    joined audio streams with echo cancellation, revved buster version

--- audio.sh
+++ audio.sh
@@ -1,1 +1,7 @@
-gst-launch-1.0 alsasrc device=plughw:0,0 ! audio/x-raw,format=S16LE,layout=interleaved,rate=48000,channels=1 ! volume volume=10 ! volume volume=3 ! opusenc ! rtpopuspay ! udpsink host=127.0.0.1 port=8005
+gst-launch-1.0 \
+rtpbin name=rtpbin latency=100 \
+udpsrc port=50000 caps="application/x-rtp, media=audio, encoding-name=OPUS, clock-rate=48000" ! rtpbin.recv_rtp_sink_0 \
+udpsrc port=50001 caps="application/x-rtcp" ! rtpbin.recv_rtcp_sink_0 \
+rtpbin. ! rtpopusdepay ! queue ! opusdec ! audioresample ! audio/x-raw,format=S16LE,layout=interleaved,rate=16000,channels=1 ! webrtcechoprobe ! alsasink \
+alsasrc device=plughw:0,0 buffer-time=20000 ! audio/x-raw,format=S16LE,layout=interleaved,rate=16000,channels=1 ! webrtcdsp noise-suppression-level=high echo-suppression-level=high ! volume volume=10 ! volume volume=3 ! \
+queue ! opusenc ! rtpopuspay ! udpsink host=127.0.0.1 port=8005
