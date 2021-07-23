d46e758f3d9feb2f840460c7c0f035b86f40b730
Nik Ivanov <2centnik@gmail.com>
2021-07-22 23:21:50 -0400

    powerplant manually sets voltage limits

--- powerplant.py
+++ powerplant.py
@@ -7,9 +7,18 @@ class PowerPlant:
         self.bus = smbus.SMBus(DEVICE_BUS)
         powerplantConfig = config["POWERPLANT"]
         cutoffVoltage = int(powerplantConfig['CutoffVoltage'])
+        fullVoltage = int(powerplantConfig['FullVoltage'])
+
+        self.bus.write_byte_data(DEVICE_ADDR, 42, 1)
+
         self.bus.write_byte_data(DEVICE_ADDR, 17, cutoffVoltage & 0xFF)
         self.bus.write_byte_data(DEVICE_ADDR, 18, (cutoffVoltage >> 8)& 0xFF)
+
+        self.bus.write_byte_data(DEVICE_ADDR, 13, fullVoltage & 0xFF)
+        self.bus.write_byte_data(DEVICE_ADDR, 14, (fullVoltage >> 8)& 0xFF)
+
         print('Set powerplant cutoff voltage to {} mv'.format(cutoffVoltage))
+        print('Set powerplant full voltage to {} mv'.format(fullVoltage))
         
 
     def getBatteryInfo(self):
@@ -18,7 +27,7 @@ class PowerPlant:
         for i in range(1,255):
             aReceiveBuf.append(self.bus.read_byte_data(DEVICE_ADDR, i))
         percentage = aReceiveBuf[20] << 8 | aReceiveBuf[19]
-        charging = (aReceiveBuf[10] << 8 | aReceiveBuf[9]) > 4000
+        charging = (aReceiveBuf[10] << 8 | aReceiveBuf[9]) > 2500
         return [percentage, charging]
 
 
