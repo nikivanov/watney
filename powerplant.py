1b321b8950870865229717784736503362c007e2
Nik Ivanov <2centnik@gmail.com>
2021-10-31 16:36:09 -0400

    changed powerplant error reporting

--- powerplant.py
+++ powerplant.py
@@ -10,13 +10,23 @@ class PowerPlant:
         self.bus.write_byte_data(DEVICE_ADDR, 17, cutoffVoltage & 0xFF)
         self.bus.write_byte_data(DEVICE_ADDR, 18, (cutoffVoltage >> 8)& 0xFF)
         print('Set powerplant cutoff voltage to {} mv'.format(cutoffVoltage))
+        self.lastIsError = False
         
 
     def getBatteryInfo(self):
-        aReceiveBuf = []
-        aReceiveBuf.append(0x00)   # Placeholder
-        for i in range(1,255):
-            aReceiveBuf.append(self.bus.read_byte_data(DEVICE_ADDR, i))
-        percentage = aReceiveBuf[20] << 8 | aReceiveBuf[19]
-        charging = (aReceiveBuf[10] << 8 | aReceiveBuf[9]) > 4000
-        return [percentage, charging]
+        try:
+            aReceiveBuf = []
+            aReceiveBuf.append(0x00)   # Placeholder
+            for i in range(1,255):
+                aReceiveBuf.append(self.bus.read_byte_data(DEVICE_ADDR, i))
+            percentage = aReceiveBuf[20] << 8 | aReceiveBuf[19]
+            charging = (aReceiveBuf[10] << 8 | aReceiveBuf[9]) > 4000
+            if self.lastIsError:
+                print('Powerplant communication restored')
+                self.lastIsError = False
+            return [percentage, charging]
+        except Exception as e:
+            if not self.lastIsError:
+                print('Error obtaining powerplant info: ' + str(e))
+                self.lastIsError = True
+            return [0, False]
